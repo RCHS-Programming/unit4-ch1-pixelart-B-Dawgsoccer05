16764af09809e7f2b3b335608d82e0afff281c11
B-Dawgsoccer05 <57766682+B-Dawgsoccer05@users.noreply.github.com>
2019-11-18 12:26:50 -0500

    Make different Pixel Colors

---
+++ resources/script.js
@@ -0,0 +1,18 @@
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor = "black";
+
+}
+
+var penColor = 'black';
+
+function setPenColor(pen)
+
+{
+    penColor = pen;
+}
+
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor = penColor;
+}
